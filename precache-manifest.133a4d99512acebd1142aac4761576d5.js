c2ae7d8eade0de24d8065ce4e0e00df5bfeb1663
Sneha Gusain <95864849+snehagusain@users.noreply.github.com>
2025-02-19 17:05:44 +0000

    Updates

---
+++ precache-manifest.133a4d99512acebd1142aac4761576d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "194cbe853b6b5ffe688d446f9a02d417",
+    "url": "/AI-based-personal-financial-advisor/index.html"
+  },
+  {
+    "revision": "7cc3db73f97e06fb5987",
+    "url": "/AI-based-personal-financial-advisor/static/css/main.4f1c7fe5.chunk.css"
+  },
+  {
+    "revision": "7cdbd9c47f1f643715d6",
+    "url": "/AI-based-personal-financial-advisor/static/js/2.4f6d30ee.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/AI-based-personal-financial-advisor/static/js/2.4f6d30ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cc3db73f97e06fb5987",
+    "url": "/AI-based-personal-financial-advisor/static/js/main.6f554fb3.chunk.js"
+  },
+  {
+    "revision": "4e8e20bcde14d10ec0be",
+    "url": "/AI-based-personal-financial-advisor/static/js/runtime-main.7728adbc.js"
+  }
+]);
